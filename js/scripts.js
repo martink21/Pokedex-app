7826c506377276f9c57935a826a27f6b9262991f
Martin Karastoyanov <martin.karastoyanov@gmail.com>
2021-04-24 12:24:36 +0200

    Added a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
 let pokemonList = [
     {
-    name: 'Charmander',
-    height: 3,
-    types: ['monster', 'dragon']
-},
+        name: 'Charmander',
+        height: 4,
+        types: ['monster', 'dragon']
+    },
 
-{
-    name: 'Pikachu',
-    height: 2,
-    types: ['field', 'fairy']
-},
+    {
+        name: 'Pikachu',
+        height: 2,
+        types: ['field', 'fairy']
+    },
 
-{
-    name: 'Squirtle',
-    height: 3,
-    types: ['monster', 'water1']
-}
+    {
+        name: 'Squirtle',
+        height: 3,
+        types: ['monster', 'water1']
+    }
 ]
 
-
-
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 3) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + ' - Wow, that\'s big!' + '<br>')
+    }
+    else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '<br>')
+    }
+}
 
 
 
